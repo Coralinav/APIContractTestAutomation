20033f4decc2a6ffd56d50c1c515819812cacd25
coralinav <mcna97@gmail.com>
2023-03-14 20:51:49 -0600

    First Commit

---
+++ src/test/java/PokeApiTest.java
@@ -0,0 +1,26 @@
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+
+public class PokeApiTest {
+
+    @Test
+    public void testPokeApi() {
+
+        // Navigate to URL
+        given()
+                .baseUri("https://pokeapi.co")
+                .log()
+                .all().
+        // Getting the endpoint
+        when()
+                .get("/api/v2/berry").
+        // Validate the JSON Schema
+        then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Berry.json"));
+    }
+}
